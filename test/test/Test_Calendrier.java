3baf21e732380a18e58ee5b21163c22e925f3e1e
Raph210 <semlali77210@gmail.com>
2015-05-17 14:16:32 +0200

    Package Test : ajout du test de l'année bisextile

--- test/test/Test_Calendrier.java
+++ test/test/Test_Calendrier.java
@@ -8,15 +8,27 @@ package test;
 import projetcalendrier.Calendrier;
 import org.junit.Test;
 import org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  *
  * @author Rasem
  */
 public class Test_Calendrier {
-    
+
     public Test_Calendrier() {
     }
 
-    
+    /**
+     * Test si le calendrier prend en compte les années bisextiles.
+     * 
+     */
+    @Test
+    public void testAnneeBisextile() {
+        model.Calendrier c1 = new model.Calendrier(2014);//Annee 2014-2015
+        model.Calendrier c2 = new model.Calendrier(2015);//Annee 2015-2016(2016 Bisextile)
+        assertEquals(365, c1.getNbjourtotal());
+        assertEquals(366, c2.getNbjourtotal());
+    }
+
 }
